f4baa01a2de4e319b9f178d59bccf8b206f765d8
Jesse Corbett <jesselcorbett@gmail.com>
2018-09-15 22:28:03 -0500

    Fix removing owner

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,21 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin');
+
 const fetch = require('node-fetch')
 
-const API_KEY = "5z4cm376tbtt9cpv3ec3yaj2"
+const API_KEY = ""
 
 const baseEtsyUrl = terms => "https://openapi.etsy.com/v2/listings/active?keywords=" + terms + "&limit=200&includes=Images:1&api_key="  + API_KEY
 const pick = size => Math.floor((Math.random() - 0.00001) * size)
 
 const topics = ["juggalo", "otherkin", "haunted doll", "anthro", "fetish"]
 const topic = () => topics[Math.floor(Math.random() * topics.length)]
+
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+db.settings(settings)
 
 exports.processNewRound = functions.firestore.document('games/{gameId}').onUpdate(change => {
   const newValues = change.after.data()
@@ -39,7 +47,8 @@ const getResult = (change, game, newPlayerStart) => fetch(baseEtsyUrl(topic())).
   })
 }).catch(() => getResult(change, game, newPlayerStart))
 
-exports.checkOwnerAck = functions.firestore.document('games/{gameId}').onUpdate(change => {
+
+exports.checkOwnerAck = functions.firestore.document('games/{gameId}').onUpdate((change, context) => {
   const newValues = change.after.data()
   const oldValues = change.before.data()
 
@@ -47,12 +56,32 @@ exports.checkOwnerAck = functions.firestore.document('games/{gameId}').onUpdate(
     return true
   }
 
-  if ((newValues.started && !oldValues.started) || (newValues.rounds.length > 0 && Object.values(newValues.rounds[newValues.rounds.length - 1].answers).filter(answer => answer === null).length === 0)) {
-    let newPlayerStart = newValues.playerStart + 1
-    if (newPlayerStart === newValues.players.length) newPlayerStart = 0
+  console.log("Checking if owner disconnected")
 
-    return getResult(change, newValues, newPlayerStart)
-  }
+  const timestamp = Date.now()
+  return db.collection('/games').doc(context.params.gameId).get().then(ref => {
+    const lastAck = ref.data().ownerAck
 
-  return true
+    console.log("Now: " + timestamp)
+    console.log("Last Ack: " + lastAck)
+
+    if (timestamp > lastAck) {
+      console.log("Owner disconnected")
+
+      if (newValues.playerCount === 1) {
+        return change.after.ref.update({
+          active: false,
+          players: []
+        })
+      }
+
+      const newPlayers = newValues.players.filter(playerId => playerId !== newValues.owner)
+      
+      return change.after.ref.update({
+        players: newPlayers,
+        playerCount: newPlayers.length,
+        owner: newPlayers[0]
+      })
+    }
+  })
 });
