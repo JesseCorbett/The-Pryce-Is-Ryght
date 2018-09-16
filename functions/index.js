98810aff0a2325144ccf4db173b71bbab2cf1352
Jesse Corbett <jesselcorbett@gmail.com>
2018-09-15 20:07:16 -0500

    Add simple retry logic to function

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ const pick = size => Math.floor((Math.random() - 0.00001) * size)
 const topics = ["juggalo", "otherkin", "haunted doll", "anthro", "fetish"]
 const topic = () => topics[Math.floor(Math.random() * topics.length)]
 
-exports.processNewRound = functions.firestore.document('games/{gameId}').onUpdate((change, context) => {
+exports.processNewRound = functions.firestore.document('games/{gameId}').onUpdate(change => {
   const newValues = change.after.data()
   const oldValues = change.before.data()
 
@@ -17,22 +17,24 @@ exports.processNewRound = functions.firestore.document('games/{gameId}').onUpdat
     const newPlayerStart = newValues.playerStart + 1
     if (newPlayerStart === newValues.players.length) newPlayerStart = 0
 
-    return fetch(baseEtsyUrl(topic())).then(result => result.json()).then(result => {
-      const picked = result.results[pick(result.results.length)]
-
-      const round = { answers: {}, subjectDescription: picked.title, subjectImage: picked.Images[0].url_fullxfull, subjectPrice: picked.price * 100, subjectUrl: picked.url }
-      newValues.players.forEach(id => round.answers[id] = null)
-
-      const rounds = newValues.rounds
-      rounds.push(round)
-
-      return change.after.ref.update({
-        rounds: rounds,
-        playerStart: newPlayerStart,
-        readyForNewRound: false
-      })
-    })
+    return getResult(newGame, newPlayerStart)
   }
 
   return true
 });
+
+const getResult = (game, newPlayerStart) => fetch(baseEtsyUrl(topic())).then(result => result.json()).then(result => {
+  const picked = result.results[pick(result.results.length)]
+
+  const round = { answers: {}, subjectDescription: picked.title, subjectImage: picked.Images[0].url_fullxfull, subjectPrice: picked.price * 100, subjectUrl: picked.url }
+  newValues.players.forEach(id => round.answers[id] = null)
+
+  const rounds = newValues.rounds
+  rounds.push(round)
+
+  return change.after.ref.update({
+    rounds: rounds,
+    playerStart: newPlayerStart,
+    readyForNewRound: false
+  })
+}).catch(() => getResult(game, newPlayerStart))
